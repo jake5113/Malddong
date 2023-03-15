f55559dc98cd26eacb1ed72ad0fd7be82281b941
jake5113 <jake5113@gmail.com>
2023-03-15 22:02:18 +0900

    - 디테일 페이지 네이버 맵 좌표 추가

--- app/src/main/java/com/jake5113/malddong/DetailActivity.java
+++ app/src/main/java/com/jake5113/malddong/DetailActivity.java
@@ -1,17 +1,31 @@
 package com.jake5113.malddong;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Camera;
 import android.os.Bundle;
 
 import com.bumptech.glide.Glide;
 import com.jake5113.malddong.databinding.ActivityDetailBinding;
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.CameraUpdate;
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.overlay.Align;
+import com.naver.maps.map.overlay.InfoWindow;
+import com.naver.maps.map.overlay.Marker;
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     ActivityDetailBinding binding;
+    LatLng latLng;
+    String toiletNm = null;
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -20,10 +34,19 @@ public class DetailActivity extends AppCompatActivity {
         binding = ActivityDetailBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        FragmentManager fm = getSupportFragmentManager();
+        MapFragment mapFragment = (MapFragment) fm.findFragmentById(R.id.detail_map);
+
+        mapFragment = MapFragment.newInstance();
+        fm.beginTransaction().replace(R.id.detail_map, mapFragment).commit();
+
+        mapFragment.getMapAsync(this);
+
         Intent intent = getIntent();
         ToiletItem toiletItem = (ToiletItem) intent.getSerializableExtra("toiletItem");
         Glide.with(this).load(toiletItem.photo[0]).into(binding.ivToilet);
-        binding.toiletNm.setText(toiletItem.toiletNm);
+        toiletNm = toiletItem.toiletNm;
+        binding.toiletNm.setText(toiletNm);
         binding.rnAdres.setText(toiletItem.rnAdres);
         binding.lnmAdres.setText(toiletItem.lnmAdres);
         binding.mngrInsttNm.setText("관리 기관:" + toiletItem.mngrInsttNm);
@@ -38,6 +61,25 @@ public class DetailActivity extends AppCompatActivity {
         binding.femaleClosetCnt.setText("여성 대변기 수:" + toiletItem.femaleClosetCnt);
         binding.femaleDspsnClosetCnt.setText("여성 장애인 대변기 수:" + toiletItem.femaleDspsnClosetCnt);
         binding.femaleChildClosetCnt.setText("여성 어린이 대변기 수:" + toiletItem.femaleChildClosetCnt);
+        latLng = new LatLng(Double.parseDouble(toiletItem.laCrdnt), Double.parseDouble(toiletItem.loCrdnt));
+    }
 
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        Marker marker = new Marker();
+        InfoWindow infoWindow = new InfoWindow();
+        infoWindow.setAdapter(new InfoWindow.DefaultTextAdapter(this) {
+            @NonNull
+            @Override
+            public CharSequence getText(@NonNull InfoWindow infoWindow) {
+                return toiletNm;
+            }
+        });
+        infoWindow.setPosition(latLng);
+        CameraUpdate cameraUpdate = CameraUpdate.scrollTo(latLng);
+        naverMap.moveCamera(cameraUpdate);
+        marker.setMap(null);
+        infoWindow.setAlpha(0.8f);
+        infoWindow.open(naverMap);
     }
 }
